771ec4a24039715484753aaeb7f6d2ad454ff976
Marian Spoiala <Marian.Spoiala@teamnet.ro>
2015-10-20 11:16:33 +0300

    LDAP will synchronize users only if ldap.synchronize=true in ldap.properties file

--- src/main/java/ro/teamnet/ldap/service/LDAPSynchronizationService.java
+++ src/main/java/ro/teamnet/ldap/service/LDAPSynchronizationService.java
@@ -7,22 +7,18 @@ import org.springframework.transaction.annotation.Transactional;
 import ro.teamnet.bootstrap.domain.Account;
 import ro.teamnet.bootstrap.service.AccountService;
 import ro.teamnet.ldap.domain.LDAPAccount;
+import ro.teamnet.ldap.util.PropertyUtil;
 import ro.teamnet.ou.domain.jpa.AccountFunction;
 import ro.teamnet.ou.domain.jpa.Organization;
 import ro.teamnet.ou.mapper.AccountMapper;
-import ro.teamnet.ou.service.AccountFunctionService;
-import ro.teamnet.ou.service.FunctionService;
-import ro.teamnet.ou.service.OrganizationAccountService;
-import ro.teamnet.ou.service.OrganizationService;
-import ro.teamnet.ou.web.rest.dto.AccountDTO;
-import ro.teamnet.ou.web.rest.dto.FunctionDTO;
-import ro.teamnet.ou.web.rest.dto.OrganizationDTO;
-import ro.teamnet.ou.web.rest.dto.PerspectiveDTO;
+import ro.teamnet.ou.service.*;
+import ro.teamnet.ou.web.rest.dto.*;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -53,6 +49,15 @@ public class LDAPSynchronizationService {
     @Inject
     AccountFunctionService accountFunctionService;
 
+    @Inject
+    PerspectiveService perspectiveService;
+
+    @Inject
+    private OUAccountService ouAccountService;
+
+    @Inject
+    PropertyUtil propertyUtil;
+
     public FunctionDTO createFunction(String code) {
         FunctionDTO functionDTO = new FunctionDTO();
         functionDTO.setActive(true);
@@ -71,15 +76,57 @@ public class LDAPSynchronizationService {
         return functionDTO;
     }
 
-    private void createOrganization() {
+    private OrganizationDTO createOrganization(String organizationCode) {
+        OrganizationDTO organizationDTO = organizationService.findByCode(organizationCode);
+        if (organizationDTO == null || organizationDTO.getId() == null) {
+            organizationDTO = new OrganizationDTO();
+            organizationDTO.setCode(organizationCode);
+            organizationDTO.setActive(true);
+            organizationDTO.setDescription(organizationCode);
+            organizationDTO = organizationService.save(organizationDTO);
+        }
 
+        return organizationDTO;
+    }
+
+    private OrganizationalUnitDTO createOrganizationalUnit(OrganizationDTO organizationDTO) {
+        Set<PerspectiveDTO> perspectiveDTOs = perspectiveService.findPerspectivesByOrganizationId(organizationDTO.getId());
+        PerspectiveDTO perspectiveDTO = null;
+        OrganizationalUnitDTO organizationalUnitDTO = null;
+        if (perspectiveDTOs == null || perspectiveDTOs.size() == 0) {
+            perspectiveDTO = new PerspectiveDTO();
+            perspectiveDTO.setCode(organizationDTO.getCode() + "_Perspective_1");
+            perspectiveDTO.setDescription(organizationDTO.getCode() + "_Perspective_1");
+            perspectiveDTO.setOrganization(organizationDTO);
+            perspectiveDTO = perspectiveService.save(perspectiveDTO);
+            organizationalUnitDTO = perspectiveDTO.getOuTreeRoot();
+        } else {
+            perspectiveDTO = perspectiveDTOs.iterator().next();
+            organizationalUnitDTO = perspectiveDTO.getOuTreeRoot();
+        }
+
+        return organizationalUnitDTO;
     }
 
     /**
-     * Function that synchronizes LDAP accounts to application accounts.
-     * PostConstruct should only be uncommented if the synchronization is needed.
+     * Checks if ldap.properties file contains the ldap.synchronize=true property.
      */
-    //@PostConstruct
+    @PostConstruct
+    private void ifSyncAccountsAllowed() {
+        try {
+            if (propertyUtil != null && propertyUtil.getProperty("ldap.synchronize").equals("true")) {
+                synchronizeAccounts();
+            }
+        } catch (NullPointerException e) {
+            log.debug("Ldap synchronize property does not exist in the configuration file! Users will not be synchronized!");
+        }
+    }
+
+    /**
+     * Method that synchronizes LDAP accounts to application accounts.
+     * Creates accounts, organizations, perspectives, root organizational units, functions.
+     * Adds functions to accounts, functions to organizational unit and accounts to organizational units.
+     */
     private void synchronizeAccounts() {
         List<LDAPAccount> ldapAccounts = ldapAccountService.findAll();
 
@@ -98,17 +145,12 @@ public class LDAPSynchronizationService {
                     String groupName = ldapAccount.getGroup();
                     String organizationCode = groupName.split("CN=")[1].split("_")[0];
 
-                    OrganizationDTO organizationDTO = organizationService.findByCode(organizationCode);
-                    if (organizationDTO == null || organizationDTO.getId() == null) {
-                        organizationDTO = new OrganizationDTO();
-                        organizationDTO.setCode(organizationCode);
-                        organizationDTO.setActive(true);
-                        organizationDTO.setDescription(organizationCode);
-                        organizationDTO = organizationService.save(organizationDTO);
-                    }
+                    //Create organization, perspective and organizational unit root if they don't exist
+                    OrganizationDTO organizationDTO = createOrganization(organizationCode);
 
-                    PerspectiveDTO perspectiveDTO = new PerspectiveDTO();
+                    OrganizationalUnitDTO organizationalUnitDTO = createOrganizationalUnit(organizationDTO);
 
+                    //Add account to the organization
                     Set<Organization> accountOrganizations = organizationAccountService.getOrgsByAccountId(existingAccount.getId());
                     Boolean exists = false;
                     for (Organization accountOrganization : accountOrganizations) {
@@ -119,11 +161,13 @@ public class LDAPSynchronizationService {
                     }
 
                     if (!exists) {
-                        Set<AccountDTO> accountDTOs = new HashSet<>();
+                        Set<AccountDTO> accountDTOs = organizationAccountService.getAccountsByOrgId(organizationDTO.getId());
+                        //Stim ca nu exista contul in organizatie datorita verificarii anterioare
                         accountDTOs.add(AccountMapper.toDTO(existingAccount));
                         organizationAccountService.createOrUpdateOrgAccounts(organizationDTO.getId(), accountDTOs);
                     }
 
+                    //If it doesn't exist, create function INSTITUTION_ADMIN or INSTITUTION_USER depending on user group
                     FunctionDTO functionDTO = null;
                     if (groupName.toLowerCase().contains("admin")) {
                         functionDTO = functionService.findOneByCode("INSTITUTION_ADMIN");
@@ -137,10 +181,31 @@ public class LDAPSynchronizationService {
                         }
                     }
 
+                    //Adaugare functie la organizational unit. Verificarea pt duplicat se face in functionService
+                    functionService.addToOrganizationalUnit(organizationalUnitDTO.getId(), functionDTO);
+
+                    //Adaugare functie la cont
                     AccountFunction accountFunction = accountFunctionService.findByAccountIdAndFunctionId(existingAccount.getId(), functionDTO.getId());
 
                     if (accountFunction == null && functionDTO != null && functionDTO.getId() != null) {
                         functionService.addToAccount(existingAccount.getId(), functionDTO);
+                    }
+
+                    //Adaugare account la organizational unit
+                    ArrayList<AccountDTO> accounts = new ArrayList<>(ouAccountService.getAccountsInOrganizationalUnit(organizationalUnitDTO.getId()));
+                    Boolean accExists = false;
+                    for (AccountDTO accountDTO : accounts) {
+                        if (accountDTO.getId() == existingAccount.getId()) {
+                            accExists = true;
+                            break;
+                        }
+                    }
+                    if (!accExists) {
+                        AccountDTO finalAccount = AccountMapper.toDTO(existingAccount);
+                        Set<FunctionDTO> functionDTOs = functionService.findAllByAccountId(finalAccount.getId());
+                        finalAccount.setFunctions(functionDTOs);
+                        accounts.add(finalAccount);
+                        ouAccountService.createOrUpdateOUAccountRelationships(organizationalUnitDTO.getId(), accounts);
                     }
                 }
             }
